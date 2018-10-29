c78de57a77a9721be3a573c2663814545b0e4d3f
Fidel Limaylla Condeso <flimaylla@MacBook-Pro-de-Fidel.local>
2018-10-29 13:18:06 -0500

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactoComponent } from './contacto/contacto.component';
+
+const rutas: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'contacto', component: ContactoComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(rutas)
   ],
   providers: [],
   bootstrap: [AppComponent]
